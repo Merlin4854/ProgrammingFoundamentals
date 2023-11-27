e062da9144e843f78c0c3b40836368545f36f990
Merlin4854 <152187653+Merlin4854@users.noreply.github.com>
2023-11-27 16:00:19 +0100

    Create MassimoValore.cs

---
+++ Lezione 1/massimovalore/MassimoValore.cs
@@ -0,0 +1,6 @@
+// See https://aka.ms/new-console-template for more information
+int V1 = 0;
+V1 = int.MaxValue;//inposta la variabile al massimo valore possibile
+V1 = V1 + 1;
+
+Console.WriteLine(V1);//il programma gestiscie l'overflow riportando il valore al minimo
